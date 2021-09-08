36b098c999c32c550e1b482ffb721f0577116b11
a00x40 <a00x401@gmail.com>
2021-09-08 12:37:05 +0200

    Constants File & Diagram Rename

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const userConstants = {
+    RECIEVE_USERS : "RECIEVE_USERS",
+    USER_LOGIN_SUCCESS: "USER_LOGIN_SUCCESS",
+    USER_LOGOUT: "USER_LOGOUT" 
+}
+
+export const questionsConstants = {
+    RECIEVE_QUESTIONS : "RECIEVE_QUESTIONS"
+}
